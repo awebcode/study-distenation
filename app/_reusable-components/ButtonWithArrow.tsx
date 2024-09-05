6057168b5b8c784ac8e2dc07ff900c343f8d4b22
awebcode <asikurrahaman997@gmail.com>
2024-09-05 14:20:48 +0600

    sales updated 2

--- app/_reusable-components/ButtonWithArrow.tsx
+++ app/_reusable-components/ButtonWithArrow.tsx
@@ -1,9 +1,10 @@
+"use client"
 import { Button } from '@/components/ui/button'
 import { cn } from '@/lib/utils';
 import { ArrowRight } from 'lucide-react'
 import Link from 'next/link'
 import React from 'react'
-
+import { motion } from "framer-motion"
 interface ButtonWithArrowProps {
     children: React.ReactNode;
     size?: "sm" | "lg" | "default" | "icon" | null;
@@ -21,7 +22,18 @@ const ButtonWithArrow: React.FC<ButtonWithArrowProps> = ({
     href = '#',
     variant="default"
 }) => {
-  return (
+    return (
+      <motion.button  initial={{ opacity: 0, y: 30, scale: 0.9, rotate: -5 }} // Initial state
+      whileInView={{ opacity: 1, y: 0, scale: 1, rotate: 0 }} // Animation to visible
+      whileTap={{ scale: 0.95, opacity: 0.8 }} // Animation on click
+      transition={{
+        duration: 0.8, // Duration for smooth effect
+        ease: [0.6, -0.05, 0.01, 0.99], // Custom ease curve
+        type: "spring", // Adds spring effect
+        stiffness: 120, // Controls bounce
+        damping: 10 // Smoothens bounce
+      }}
+      viewport={{ once: true }}>
       <Button size={size}
           variant={variant}
           onClick={onClick}
@@ -30,7 +42,8 @@ const ButtonWithArrow: React.FC<ButtonWithArrowProps> = ({
               className
           )}>
           <Link className={href} href="#"> <span>{children}</span></Link> <ArrowRight className="w-4 h-4 group-hover:translate-x-1  duration-300" />
-      </Button>
+            </Button>
+        </motion.button>
   )
 }
 
